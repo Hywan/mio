475a3de15f239481c2399fcb25875308c44e9676
Ivan Enderlin <ivan@mnt.io>
2021-04-20 15:36:45 +0200

    Implement `tcp::accept`.

--- src/sys/wasm/mod.rs
+++ src/sys/wasm/mod.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 pub(crate) mod wasi {
     // Code is copied from `wasmer-wasi`.
     #![allow(non_camel_case_types)]
@@ -59,6 +61,82 @@ pub(crate) mod wasi {
 
     pub type __wasi_errno_t = u16;
     pub const __WASI_ESUCCESS: u16 = 0;
+    pub const __WASI_E2BIG: u16 = 1;
+    pub const __WASI_EACCES: u16 = 2;
+    pub const __WASI_EADDRINUSE: u16 = 3;
+    pub const __WASI_EADDRNOTAVAIL: u16 = 4;
+    pub const __WASI_EAFNOSUPPORT: u16 = 5;
+    pub const __WASI_EAGAIN: u16 = 6;
+    pub const __WASI_EALREADY: u16 = 7;
+    pub const __WASI_EBADF: u16 = 8;
+    pub const __WASI_EBADMSG: u16 = 9;
+    pub const __WASI_EBUSY: u16 = 10;
+    pub const __WASI_ECANCELED: u16 = 11;
+    pub const __WASI_ECHILD: u16 = 12;
+    pub const __WASI_ECONNABORTED: u16 = 13;
+    pub const __WASI_ECONNREFUSED: u16 = 14;
+    pub const __WASI_ECONNRESET: u16 = 15;
+    pub const __WASI_EDEADLK: u16 = 16;
+    pub const __WASI_EDESTADDRREQ: u16 = 17;
+    pub const __WASI_EDOM: u16 = 18;
+    pub const __WASI_EDQUOT: u16 = 19;
+    pub const __WASI_EEXIST: u16 = 20;
+    pub const __WASI_EFAULT: u16 = 21;
+    pub const __WASI_EFBIG: u16 = 22;
+    pub const __WASI_EHOSTUNREACH: u16 = 23;
+    pub const __WASI_EIDRM: u16 = 24;
+    pub const __WASI_EILSEQ: u16 = 25;
+    pub const __WASI_EINPROGRESS: u16 = 26;
+    pub const __WASI_EINTR: u16 = 27;
+    pub const __WASI_EINVAL: u16 = 28;
+    pub const __WASI_EIO: u16 = 29;
+    pub const __WASI_EISCONN: u16 = 30;
+    pub const __WASI_EISDIR: u16 = 31;
+    pub const __WASI_ELOOP: u16 = 32;
+    pub const __WASI_EMFILE: u16 = 33;
+    pub const __WASI_EMLINK: u16 = 34;
+    pub const __WASI_EMSGSIZE: u16 = 35;
+    pub const __WASI_EMULTIHOP: u16 = 36;
+    pub const __WASI_ENAMETOOLONG: u16 = 37;
+    pub const __WASI_ENETDOWN: u16 = 38;
+    pub const __WASI_ENETRESET: u16 = 39;
+    pub const __WASI_ENETUNREACH: u16 = 40;
+    pub const __WASI_ENFILE: u16 = 41;
+    pub const __WASI_ENOBUFS: u16 = 42;
+    pub const __WASI_ENODEV: u16 = 43;
+    pub const __WASI_ENOENT: u16 = 44;
+    pub const __WASI_ENOEXEC: u16 = 45;
+    pub const __WASI_ENOLCK: u16 = 46;
+    pub const __WASI_ENOLINK: u16 = 47;
+    pub const __WASI_ENOMEM: u16 = 48;
+    pub const __WASI_ENOMSG: u16 = 49;
+    pub const __WASI_ENOPROTOOPT: u16 = 50;
+    pub const __WASI_ENOSPC: u16 = 51;
+    pub const __WASI_ENOSYS: u16 = 52;
+    pub const __WASI_ENOTCONN: u16 = 53;
+    pub const __WASI_ENOTDIR: u16 = 54;
+    pub const __WASI_ENOTEMPTY: u16 = 55;
+    pub const __WASI_ENOTRECOVERABLE: u16 = 56;
+    pub const __WASI_ENOTSOCK: u16 = 57;
+    pub const __WASI_ENOTSUP: u16 = 58;
+    pub const __WASI_ENOTTY: u16 = 59;
+    pub const __WASI_ENXIO: u16 = 60;
+    pub const __WASI_EOVERFLOW: u16 = 61;
+    pub const __WASI_EOWNERDEAD: u16 = 62;
+    pub const __WASI_EPERM: u16 = 63;
+    pub const __WASI_EPIPE: u16 = 64;
+    pub const __WASI_EPROTO: u16 = 65;
+    pub const __WASI_EPROTONOSUPPORT: u16 = 66;
+    pub const __WASI_EPROTOTYPE: u16 = 67;
+    pub const __WASI_ERANGE: u16 = 68;
+    pub const __WASI_EROFS: u16 = 69;
+    pub const __WASI_ESPIPE: u16 = 70;
+    pub const __WASI_ESRCH: u16 = 71;
+    pub const __WASI_ESTALE: u16 = 72;
+    pub const __WASI_ETIMEDOUT: u16 = 73;
+    pub const __WASI_ETXTBSY: u16 = 74;
+    pub const __WASI_EXDEV: u16 = 75;
+    pub const __WASI_ENOTCAPABLE: u16 = 76;
 
     pub type __wasi_filesize_t = u64;
 
@@ -209,7 +287,7 @@ cfg_os_poll! {
                             None
                         }
                     })
-                    .ok_or_else(|| io_err!(format!("Cannot deregister the file descriptor `{:?}` because it is not registered", fd)))?;
+                    .ok_or_else(|| io_err!(format!("Cannot deregister the file descriptor `{}` because it is not registered", fd)))?;
 
                 let _value = register.remove(index);
 
@@ -240,7 +318,7 @@ cfg_os_poll! {
                         } else if interests.is_writable() {
                             __WASI_EVENTTYPE_FD_WRITE
                         } else {
-                            return Err(io_err!(format!("Interest for file descriptor `{:?}` and token `{:?}` is not supported", fd, token)));
+                            return Err(io_err!(format!("Interest for file descriptor `{}` and token `{:?}` is not supported", fd, token)));
                         },
                         u: __wasi_subscription_u {
                             fd_readwrite: __wasi_subscription_fs_readwrite_t {
@@ -277,11 +355,11 @@ cfg_os_poll! {
                 ) };
 
                 if result != __WASI_ESUCCESS {
-                    return Err(io_err!(format!("Calling `poll_oneoff` returned `{:?}` (i.e. not a success)", result)));
+                    return Err(io_err!(format!("Calling `poll_oneoff` returned `{}` (i.e. not a success)", result)));
                 }
 
                 if wasi_events_len != wasi_events.len() as u32 {
-                    return Err(io_err!(format!("Unexpected number of events (expected `{:?}`, received `{:?}`)", wasi_events.len(), wasi_events_len)));
+                    return Err(io_err!(format!("Unexpected number of events (expected `{}`, received `{}`)", wasi_events.len(), wasi_events_len)));
                 }
 
                 *events = wasi_events
@@ -294,7 +372,7 @@ cfg_os_poll! {
                             interest: match wasi_event.type_ {
                                 __WASI_EVENTTYPE_FD_READ => Interest::READABLE,
                                 __WASI_EVENTTYPE_FD_WRITE => Interest::WRITABLE,
-                                ty => return Err(io_err!(format!("Invalid `__wasi_event_t.type_` value `{:?}`", ty))),
+                                ty => return Err(io_err!(format!("Invalid `__wasi_event_t.type_` value `{}`", ty))),
                             },
                             token: Token(wasi_event.userdata.try_into().map_err(|e: TryFromIntError| io_err!(e.to_string()))?),
                         })
@@ -380,7 +458,7 @@ cfg_os_poll! {
             use std::time::Duration;
 
             pub use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr, Shutdown, SocketAddrV4, SocketAddrV6};
-            pub use wasio::sys::{socket_create, socket_bind, socket_listen};
+            pub use wasio::sys::{socket_create, socket_bind, socket_listen, socket_pre_accept, socket_accept, wait as socket_wait};
 
             #[derive(Debug)]
             #[allow(unused)]
@@ -398,7 +476,7 @@ cfg_os_poll! {
 
             #[derive(Debug)]
             pub struct TcpListener {
-                socket: TcpSocket,
+                pub(in crate::sys) socket: TcpSocket,
             }
 
             impl TcpListener {
@@ -450,9 +528,16 @@ cfg_os_poll! {
             }
 
             #[derive(Debug)]
-            pub struct TcpStream;
+            pub struct TcpStream {
+                socket: TcpSocket,
+                address: SocketAddr,
+            }
 
             impl TcpStream {
+                pub(in crate::sys) fn new(socket: TcpSocket, address: SocketAddr) -> Self {
+                    Self { socket, address }
+                }
+
                 pub fn connect<A: ToSocketAddrs>(_addr: A) -> io::Result<Self> {
                     todo!("`TcpStream::connect`");
                 }
@@ -563,12 +648,13 @@ cfg_os_poll! {
 
         pub(crate) mod tcp {
             use crate::net::TcpKeepalive;
-            use crate::sys::net::{socket_create, socket_bind, socket_listen};
-            use crate::sys::wasi::__wasi_fd_t;
+            use crate::sys::net::{socket_create, socket_bind, socket_listen, socket_pre_accept, socket_accept, socket_wait};
+            use crate::sys::wasi::{__wasi_fd_t, __WASI_EAGAIN};
             use std::io;
-            use std::net::SocketAddr;
+            use std::mem;
+            use std::net::{SocketAddr, IpAddr, Ipv4Addr};
             use std::time::Duration;
-            use wasio::types::{AF_INET, AF_INET6, SOCK_STREAM, SockaddrIn};
+            use wasio::types::{AF_INET, AF_INET6, SOCK_STREAM, SockaddrIn, CancellationToken, UserContext};
 
             pub use crate::sys::net::{TcpListener, TcpStream};
             pub type TcpSocket = __wasi_fd_t;
@@ -639,8 +725,55 @@ cfg_os_poll! {
                 Ok(TcpListener::new(socket))
             }
 
-            pub fn accept(_listener: &TcpListener) -> io::Result<(TcpStream, SocketAddr)> {
-                todo!("`tcp::accept`");
+            pub fn accept(listener: &TcpListener) -> io::Result<(TcpStream, SocketAddr)> {
+                let mut cancellation_token = CancellationToken(0);
+                let mut user_context = UserContext(0);
+
+                let mut err = unsafe {
+                    socket_pre_accept(
+                        listener.socket,
+                        user_context,
+                        &mut cancellation_token,
+                    )
+                };
+
+                if err != 0 {
+                    return Err(io_err!(format!("`tcp::socket_pre_accept` failed with `{}`", err)));
+                }
+
+                err = 0;
+                unsafe { socket_wait(&mut err, &mut user_context) };
+
+                if err != 0 {
+                    return Err(io_err!(format!("`tcp::socket_wait` failed with `{}`", err)));
+                }
+
+                let mut connection: __wasi_fd_t = 0;
+                let mut address = SockaddrIn::default();
+                let err = unsafe {
+                    socket_accept(
+                        &mut connection,
+                        &mut address as *mut _ as *mut u8,
+                        mem::size_of::<SockaddrIn>() as u32,
+                    )
+                };
+
+                if err != 0 && err != __WASI_EAGAIN {
+                    return Err(io_err!(format!("`tcp::socket_accept` failed with `{}`", err)));
+                }
+
+                let port = address.sin_port;
+
+                #[cfg(target_endian = "little")]
+                let port = port.to_le();
+
+                let socket: TcpSocket = connection;
+                let address = SocketAddr::new(IpAddr::V4(address.sin_addr.into()), port);
+
+                dbg!(&socket);
+                dbg!(&address);
+
+                Ok((TcpStream::new(socket, address), address))
             }
 
             pub fn set_reuseaddr(_socket: TcpSocket, _reuseaddr: bool) -> io::Result<()> {
